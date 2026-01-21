6ff3058db30617a562494f7726d7edc744141f9e
Beforerr <zzj956959688@gmail.com>
2026-01-21 00:40:54 -0800

    feat: improve IGRF interpolation accuracy

--- test/runtests.jl
+++ test/runtests.jl
@@ -58,20 +58,6 @@ end
     @info @b GeoCotrans.csundir($dt)
 end
 
-@testitem "IGRF get_B" begin
-    using Dates
-
-    # Test IGRF magnetic field calculation
-    r, θ, φ = 6500.0, 30.0, 4.0
-    t = Date(2021, 3, 28)
-    B_true = (-46077.31133522, -14227.12618499, 233.14355744)
-    @test all(igrf_Bd(r, θ, φ, t) .≈ B_true)
-
-    𝐫 = GDZ(0, 60.39299, 5.32415)
-    B_true = (458.89660058, 14996.72893889, -49019.55372591)
-    @test all(igrf_B(𝐫, t) .≈ B_true)
-end
-
 @testitem "Aqua" begin
     using Aqua
     Aqua.test_all(GeoCotrans)
