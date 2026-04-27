41dbf951ee0e65c03eda17dc9db853033922c90d
Beforerr <zzj956959688@gmail.com>
2026-04-27 07:59:31 -0700

    feat: add matrix support for MLT calculation with DimensionalData integration

--- src/mlt.jl
+++ src/mlt.jl
@@ -1,3 +1,5 @@
+φ(r) = (tmp = atan(r[2], r[1]); tmp < 0 ? tmp + 2π : tmp)
+
 """
     get_mlt(xGEO, time)
 
@@ -7,9 +9,7 @@ MLT is computed from the difference between the magnetic longitudes of the posit
 
 - [IRBEM implementation](https://github.com/IRBEM/IRBEM/blob/5c2c6c2/src/geo_tran.f)
 """
-function get_mlt(xGEO, time)
-    φ(r) = (tmp = atan(r[2], r[1]); tmp < 0 ? tmp + 2π : tmp)
-
+@inline function get_mlt(xGEO, time)
     x_geo = GEO(xGEO, time)
     x_mag = MAG(x_geo)
     mlon_pos = φ(x_mag)
@@ -20,4 +20,9 @@ function get_mlt(xGEO, time)
 
     mlt = rad2deg(mlon_pos - mlon_sun) / 15 + 12
     return mod(mlt, 24)
+end
+
+@inline function get_mlt(xGEO::AbstractMatrix, times; dim)
+    out = similar(xGEO, axes(xGEO, dim))
+    return out .= get_mlt.(eachslice(xGEO; dims = dim), times)
 end
